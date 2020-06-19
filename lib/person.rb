f17c85636ef35d843e088f157286eaa6743caed7
Jonathan Jopio <jonathanjopio@gmail.com>
2020-06-19 03:22:16 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
